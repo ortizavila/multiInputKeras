71573cdaad2f366134d8520599fe72bbe3e216f9
Alfonso Ortiz Avila <130321366+ortizavila@users.noreply.github.com>
2023-10-06 01:13:12 -0600

    Update kerasModel.py

--- kerasModel.py
+++ kerasModel.py
@@ -28,3 +28,17 @@ z = Dense(1, activation="linear")(z)
 # then output a single value
 model = Model(inputs=[x.input, y.input], outputs=z)
 
+# compile the model using mean absolute percentage error as our loss,
+# implying that we seek to minimize the absolute percentage difference
+# between our price *predictions* and the *actual prices*
+opt = Adam(lr=1e-3, decay=1e-3 / 200)
+model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
+# train the model
+print("[INFO] training model...")
+model.fit(
+	x=[trainAttrX, trainImagesX], y=trainY,
+	validation_data=([testAttrX, testImagesX], testY),
+	epochs=200, batch_size=8)
+# make predictions on the testing data
+print("[INFO] predicting house prices...")
+preds = model.predict([testAttrX, testImagesX])
